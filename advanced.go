b153320d3ee9dbab2df4f08c69e7f4e7c69b4a4a
Josh Mills <josh@trueheart78.com>
2018-08-16 11:19:22 -0400

    advanced bbolt

--- advanced.go
+++ advanced.go
@@ -3,70 +3,108 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	bolt "github.com/coreos/bbolt"
 )
 
-var db *bolt.DB
 var bucketName = "gifs"
+var db = initDB()
 
 type Gif struct {
 	ID       string `json:"checksum"`
 	BaseName string `json:"base_name"`
 	FileSize int    `json:"file_size"`
+	Count    int    `json:"count"`
 }
 
 func main() {
 	var gifs []Gif
-	gifs = append(gifs, Gif{"abc", "taylor.gif", 123})
-	gifs = append(gifs, Gif{"def", "swift.gif", 234})
-	gifs = append(gifs, Gif{"ghi", "rocks.gif", 345})
+	gifs = append(gifs, Gif{"abc", "taylor.gif", 123, 0})
+	gifs = append(gifs, Gif{"def", "swift.gif", 234, 0})
+	gifs = append(gifs, Gif{"ghi", "rocks.gif", 345, 1})
 
+	// save all data
+	for _, g := range gifs {
+		_, err := g.save()
+		if err != nil {
+			fmt.Println(err)
+		}
+		for x := 0; x < 10; x++ {
+			_, err = g.increment()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	// TODO: See how many records exist?
+	// retrieve all data
+	for _, g := range gifs {
+		a, err := find(g.ID)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(a)
+	}
+
+	a, _ := find("abc")
+	fmt.Println(a)
+}
+
+func initDB() *bolt.DB {
 	os.Remove("data/test.db")
 	db, err := bolt.Open("data/test.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+	} else {
+		initBucket(db)
 	}
+	return db
+}
 
-	// create a bucket!
-	err = db.Update(func(tx *bolt.Tx) error {
+// create the bucket
+func initBucket(db *bolt.DB) {
+	err := db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists([]byte(bucketName))
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-		return nil
+		return err
 	})
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
+}
 
-	for _, g := range gifs {
-		data, err := json.Marshal(g)
-		if err != nil {
-			fmt.Println(err)
-		}
-		// save some data!
-		err = db.Update(func(tx *bolt.Tx) error {
-			b := tx.Bucket([]byte(bucketName))
-			err := b.Put([]byte(g.ID), data)
-			return err
-		})
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+func (g Gif) json() []byte {
+	data, _ := json.Marshal(g)
+	return data
+}
 
-	for _, g := range gifs {
-		err = db.View(func(tx *bolt.Tx) error {
-			b := tx.Bucket([]byte(bucketName))
-			v := b.Get([]byte(g.ID))
-			fmt.Printf("%s\n", v)
-			return nil
-		})
-		if err != nil {
-			fmt.Println(err)
-		}
+func (g *Gif) increment() (bool, error) {
+	g.Count++
+	return g.save()
+}
+
+func (g Gif) save() (bool, error) {
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		err := b.Put([]byte(g.ID), g.json())
+		return err
+	})
+	if err != nil {
+		return false, err
 	}
+	return true, nil
+}
+
+func find(checksum string) (Gif, error) {
+	var gif Gif
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		v := b.Get([]byte(checksum))
+		json.Unmarshal(v, &gif)
+		return nil
+	})
+	return gif, err
 }
